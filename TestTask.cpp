17c1d49ea4e683c5412b5ce91e77a09d5a1ba185
unknown <s.hashimali512@gmail.com>
2018-01-20 20:56:02 +0500

    Done with reduce fun

--- TestTask.cpp
+++ TestTask.cpp
@@ -26,12 +26,29 @@ vector<T> map(const vector<T>&v)
 	return map_sqr_Func(square<T>, v);
 }
 
+template<typename T>
+T reducefn(T result, T value)
+{
+	return (result + value);
+}
+
+template<typename T>
+T reduce(const vector<T>& v)
+{
+	T result = v.at(1) - v.at(1);
+	for (T item : v)
+	{
+		result = reducefn(result, item);
+	}
+	return result;
+}
 
 
 
 
 void main()
 {
+
 	vector<double> v({ 1.1,2.2,3,3.3 });
 	cout << "after map function" << endl;
 	v = map(v);
@@ -39,4 +56,13 @@ void main()
 	{
 		cout << v.at(i) << endl;
 	}
+	vector<double> v1({ 1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9,10 });
+	/*for (int i = 0; i <= v1.size() - 1; i++)
+	{
+	cout << v1.at(i) << endl;
+	}*/
+	cout << "After reduce" << endl;
+	cout << reduce(v1) << endl;
+
+
 }
